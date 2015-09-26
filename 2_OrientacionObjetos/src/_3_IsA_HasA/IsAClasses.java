c6704a754bff1fde50cad593edea6d7274b7c33f
Chris <crimarde@gmail.com>
2015-09-26 14:12:05 +0200

    Añadidas las clases para IsA

---
+++ 2_OrientacionObjetos/src/_3_IsA_HasA/IsAClasses.java
@@ -0,0 +1,46 @@
+package _3_IsA_HasA;
+
+public class IsAClasses {
+	
+	public static void main(String... args){
+		Vehiculo v = new Vehiculo();
+		Vehiculo vc = new Coche();
+		Vehiculo vf = new Ford();
+		
+		Coche c = new Coche();
+		Coche cf = new Ford();
+		
+		Ford f = new Ford();
+		
+		System.out.println("Todos heredan de vehiculo o una subclase, por tanto todos \"IsA\" vehiculo:");
+		System.out.println("v isA Vehiculo -> " + String.valueOf(v instanceof Vehiculo));
+		System.out.println("vc isA Vehiculo -> " + String.valueOf(vc instanceof Vehiculo));
+		System.out.println("vf isA Vehiculo -> " + String.valueOf(vf instanceof Vehiculo));
+		System.out.println("c isA Vehiculo -> " + String.valueOf(c instanceof Vehiculo));
+		System.out.println("cf isA Vehiculo -> " + String.valueOf(cf instanceof Vehiculo));
+		System.out.println("f isA Vehiculo -> " + String.valueOf(f instanceof Vehiculo));
+		
+		System.out.println("\nLos que no son:");
+		System.out.println("c isA Ford -> " + String.valueOf(c instanceof Ford));
+		System.out.println("c isA Coche -> " + String.valueOf(v instanceof Coche));
+	}
+	
+}
+
+class Vehiculo{
+	public void identificame(){
+		System.out.println("Soy un vehiculo");
+	}
+}
+
+class Coche extends Vehiculo{
+	public void identificame(){
+		System.out.println("Soy un coche");
+	}
+}
+
+class Ford extends Coche{
+	public void identificame(){
+		System.out.println("Soy un Ford");
+	}
+}
