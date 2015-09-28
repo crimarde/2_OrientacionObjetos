c4b4a9fce44dcc3ced6083332b8b5ef8928f64e1
Chris <crimarde@gmail.com>
2015-09-29 00:29:12 +0200

    Pequeñas actualizaciones de código

--- 2_OrientacionObjetos/src/_2_Herencia_y_Polimirfismo/Herencia1.java
+++ 2_OrientacionObjetos/src/_2_Herencia_y_Polimirfismo/Herencia1.java
@@ -13,13 +13,14 @@ public class Herencia1 {
 		//Pasar un vehiculo a una clase
 		Vehiculo v = new Vehiculo();
 		Coche c = new Coche();
+		System.out.println("Llamadas al metodo pinta acepta un vehiculo como parametro, pasandole un vehiculo y un coche (extiende de vehiculo)");
 		pinta(v);
 		pinta(c); //Puedo pasar un coche porque hereda de vehiculo (es un vehiculo)
 		//Solo se puede llamar a los metodos de vehiculo y no a ningun metodo de las subclases
 	}
 	
 	public static void pinta(Vehiculo v){  //Acepta Vehiculo o cualquier subtipo
-		v.arranca();
+		System.out.println("Soy un objeto de la clase " + v.getClass().getSimpleName().toString());
 	}
 }
 
